95a4a5b57d2473f758b8d98525e9ad50615af606
Jason A. DeMartino <141532276+Jaycode1998@users.noreply.github.com>
2023-08-27 16:37:20 -0400

    Add files via upload

---
+++ Games.py
@@ -0,0 +1,18 @@
+print("Welcome to Econ 101")
+
+playing = input("Do you want to play? ")
+if playing != "yes":
+    quit()
+print("Okay Let's Get Our Rothbard On!")
+
+answer = input("What is the necessarily impersonal integer used to define exchange ratios? ")
+if answer == "prices":
+   print("Correct!") 
+else:
+    print("Sorry that wasn't right :(  ")
+
+answer = input("Who Wrote Economic Calculation in the Socialist Commonwealth? ")
+if answer == "Ludwig von Mises":
+   print("Correct!") 
+else:
+    print("Sorry that wasn't right :(  ")
